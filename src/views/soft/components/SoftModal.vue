69d2be256f1c0223c4226c451a3e9677d639f5ab
fuxin <fx2663909437@163.com>
2023-02-22 10:51:56 +0800

    fix: edit-fixed

--- src/views/soft/components/SoftModal.vue
+++ src/views/soft/components/SoftModal.vue
@@ -1,18 +1,19 @@
 <template>
 	<a-modal
-		:id="editId"
 		:visible="visible"
 		@ok="onSubmit"
-		@cancel="closeModal(false)"
+		@cancel="closeModal"
 		:maskClosable="false"
 		:keyboard="false"
-		:confirmLoading="loading"
 		:width="550"
 		:title="title ? '编辑软件模块' : '新增软件模块'"
 	>
 		<a-form :label-col="{ span: 5 }" :wrapper-col="{ span: 14 }" :form="form" v-bind="formItemLayout">
 			<a-form-item label="标题">
-				<a-input placeholder="请输入标题" v-decorator="['title']" />
+				<a-input
+					placeholder="请输入标题"
+					v-decorator="['title',  {rules:[ { required: true, message: '标题必填!' }]}] "
+				/>
 			</a-form-item>
 			<a-form-item label="副标题">
 				<a-input placeholder="请输入副标题" v-decorator="['subheading']" />
@@ -21,7 +22,11 @@
 				<a-textarea placeholder="请输入描述" :rows="4" v-decorator="['content']" />
 			</a-form-item>
 			<a-form-item label="内容标签" extra="标签之间用英文逗号隔开">
-				<a-textarea placeholder="请输入内容标签" :rows="4" v-decorator="['contentDescription']" />
+				<a-textarea
+					placeholder="请输入内容标签"
+					:rows="4"
+					v-decorator="['contentDescription',{rules:[{required:true,message:'内容标签必填!'}]}]"
+				/>
 			</a-form-item>
 			<a-form-item label="链接">
 				<a-input placeholder="请输入链接" :rows="4" v-decorator="['link']" />
@@ -31,7 +36,7 @@
 			</a-form-item>
 			<a-form-item label="附件" extra="此处上传产品板块轮播图">
 				<a-upload
-					v-decorator="['upload',{valuePropName: 'fileList'}]"
+					v-decorator="['attachment',{valuePropName: 'fileList'}]"
 					action="http://114.67.199.59/cfc/file/upload"
 					list-type="text"
 				>
@@ -46,12 +51,7 @@
 
 <script>
 import ATextarea from 'ant-design-vue/es/input/TextArea'
-import {
-	addSoft,
-	updateSoft,
-	getSoftInfoList,
-	getSoftInfo
-} from '@/api/soft.js'
+import { addSoft, updateSoft } from '@/api/soft.js'
 export default {
 	components: { ATextarea },
 	props: {
@@ -63,57 +63,79 @@ export default {
 			type: Boolean,
 			required: true
 		},
-		id: {
-			type: String
+		data: {
+			type: Object
 		}
 	},
 	data() {
 		return {
 			form: this.$form.createForm(this),
-			loading: false,
 			formItemLayout: {
 				labelCol: { span: 6 },
 				wrapperCol: { span: 18 }
 			}
 		}
 	},
+	watch: {
+		visible() {
+			setTimeout(() => {
+				this.getFormData()
+			}, 500)
+		}
+	},
 	methods: {
-		async getInfo() {
-			if (this.editId) {
-				const res = await getSoftInfo(this.editId)
-				console.log(res, 'res')
-			} else {
-				console.log('没有找到id')
-				return
-			}
-		},
-		async getList() {
-			const code = 'SOFTWARE_PRODUCT'
-			const res = await getSoftInfoList(code)
-			this.dataSource = res
+		getFormData() {
+			const {
+				title,
+				subheading,
+				content,
+				contentDescription,
+				link,
+				showIndex,
+				attachment
+			} = this.data
+			this.form.setFieldsValue({
+				title,
+				subheading,
+				content,
+				contentDescription,
+				link,
+				showIndex,
+				attachment
+			})
 		},
 		onSubmit() {
 			this.form.validateFields(async (err, values) => {
 				if (err) return
-				console.log(values, 'values')
-				this.loading = true
-				values.contentDescription = values.contentDescription.split(',')
-				const params = {
-					...values,
-					code: 'SOFTWARE_PRODUCT'
+				if (values.contentDescription == ' ') {
+					return (values.contentDescription = [])
+				} else {
+					values.contentDescription = values.contentDescription
+						.split(',')
+						.filter(item => {
+							if (item != ' ') {
+								return item
+							}
+						})
 				}
 				if (!this.title) {
-					const res = await addSoft(params)
+					const assParams = {
+						...values,
+						code: 'SOFTWARE_PRODUCT'
+					}
+					const res = await addSoft(assParams)
 					if (res.message == 'success') {
-						this.closeModal()
-						this.getList()
 						this.$message.success('新增成功')
+						this.$emit('ok')
 					}
 				} else {
-					const res = await updateSoft(params)
+					const updParams = {
+						...values,
+						id: this.data.id
+					}
+					const res = await updateSoft(updParams)
 					if (res.message == 'success') {
-						this.closeModal()
-						this.getList()
+						this.$emit('ok')
 						this.$message.success('修改成功')
 					}
 				}
@@ -122,7 +144,6 @@ export default {
 		closeModal() {
 			this.form.resetFields()
 			this.$emit('close')
-			this.title = false
 		}
 	}
 }
--- src/views/soft/soft.vue
+++ src/views/soft/soft.vue
@@ -8,7 +8,6 @@
 				:dataSource="dataSource"
 				rowKey="name"
 				:pagination="false"
-				:loading="tableLoading"
 			>
 				<span
 					slot="updateTime"
@@ -16,71 +15,27 @@
 				>{{ new Date(updateTime) | formatDate('yyyy-MM-dd') }}</span>
 
 				<div slot="actions" slot-scope="record">
-					<a @click="onEditContent(record.id)">编辑</a>
+					<a @click="onEditContent(record)">编辑</a>
 					<a-divider type="vertical" />
 					<a @click="onDeleteContent(record.id)">删除</a>
 				</div>
 			</a-table>
+			<soft-modal
+				:visible="visible"
+				:data="formData"
+				:title="title"
+				:module="selectModule"
+				@close="onModalClose"
+				@ok="handleOk"
+			></soft-modal>
 		</a-card>
-		<a-modal
-			:visible="visible"
-			@ok="onSubmit"
-			@cancel="closeModal"
-			:maskClosable="false"
-			:keyboard="false"
-			:confirmLoading="loading"
-			:width="550"
-			:title="title ? '编辑软件模块' : '新增软件模块'"
-		>
-			<a-form
-				:label-col="{ span: 5 }"
-				:wrapper-col="{ span: 14 }"
-				:form="form"
-				v-bind="formItemLayout"
-			>
-				<a-form-item label="标题">
-					<a-input placeholder="请输入标题" v-decorator="['title']" />
-				</a-form-item>
-				<a-form-item label="副标题">
-					<a-input placeholder="请输入副标题" v-decorator="['subheading']" />
-				</a-form-item>
-				<a-form-item label="内容">
-					<a-textarea placeholder="请输入描述" :rows="4" v-decorator="['content']" />
-				</a-form-item>
-				<a-form-item label="内容标签" extra="标签之间用英文逗号隔开">
-					<a-textarea placeholder="请输入内容标签" :rows="4" v-decorator="['contentDescription']" />
-				</a-form-item>
-				<a-form-item label="链接">
-					<a-input placeholder="请输入链接" :rows="4" v-decorator="['link']" />
-				</a-form-item>
-				<a-form-item label="显示顺序">
-					<a-input placeholder="请输入显示顺序" :rows="4" v-decorator="['showIndex']" />
-				</a-form-item>
-				<a-form-item label="附件" extra="此处上传产品板块轮播图">
-					<a-upload
-						v-decorator="['upload',{valuePropName: 'fileList'}]"
-						action="http://114.67.199.59/cfc/file/upload"
-						list-type="text"
-					>
-						<a-button type="primary">
-							<a-icon type="upload" />点击上传
-						</a-button>
-					</a-upload>
-				</a-form-item>
-			</a-form>
-		</a-modal>
 	</div>
 </template>
 
 <script>
+import SoftModal from './components/SoftModal'
 import { tableMixin } from '@/mixins'
-import {
-	getSoftInfoList,
-	deleteSoft,
-	getSoftInfo,
-	updateSoft,
-	addSoft
-} from '@/api/soft.js'
+import { getSoftInfoList, deleteSoft } from '@/api/soft.js'
 
 const columns = [
 	{
@@ -112,7 +67,7 @@ const columns = [
 		align: 'center'
 	},
 	{
-		title: '内容描述',
+		title: '内容标签',
 		dataIndex: 'contentDescription',
 		align: 'center'
 	},
@@ -138,6 +93,7 @@ const columns = [
 ]
 
 export default {
+	components: { SoftModal },
 	mixins: [tableMixin],
 	data() {
 		return {
@@ -147,10 +103,9 @@ export default {
 				wrapperCol: { span: 18 }
 			},
 			form: this.$form.createForm(this),
-			loading: false,
 			visible: false,
 			title: false,
-			id: ''
+			formData: {}
 		}
 	},
 	methods: {
@@ -170,18 +125,15 @@ export default {
 			this.title = false
 		},
 		/* 关闭Modal */
-		closeModal() {
-			this.form.resetFields()
+		onModalClose() {
 			this.title = false
 			this.visible = false
 		},
 		/* 编辑Modal */
-		async onEditContent(id) {
+		onEditContent(data) {
+			this.formData = data
 			this.visible = true
 			this.title = true
-			this.id = id
-			const res = await getSoftInfo(id)
-			this.form.setFieldsValue(res)
 		},
 		/* 删除 */
 		async onDeleteContent(id) {
@@ -201,43 +153,10 @@ export default {
 				}
 			})
 		},
-		/* 确定 */
-		onSubmit() {
-			this.form.validateFields(async (err, values) => {
-				if (err) return
-				this.loading = true
-
-				if (!this.title) {
-					values.contentDescription = values.contentDescription
-						.split(',')
-						.filter(item => {
-							if (item != ' ') {
-								return item
-							}
-						})
-					const assParams = {
-						...values,
-						code: 'SOFTWARE_PRODUCT'
-					}
-					const res = await addSoft(assParams)
-					this.closeModal()
-					if (res.message == 'success') {
-						this.getList()
-						this.$message.success('新增成功')
-					}
-				} else {
-					const delParams = {
-						...values,
-						id: this.id
-					}
-					const res = await updateSoft(delParams)
-					this.closeModal()
-					if (res.message == 'success') {
-						this.getList()
-						this.$message.success('修改成功')
-					}
-				}
-			})
+		/* 提交成功 */
+		handleOk() {
+			this.getList()
+			this.onModalClose()
 		}
 	},
 	created() {
