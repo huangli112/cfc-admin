9eee1d398cc998ea962a44eaeb441cca15b76c40
huangli112 <huangli112@126.com>
2023-02-23 09:51:06 +0800

    fix: soft

--- src/views/soft/components/SoftModal.vue
+++ src/views/soft/components/SoftModal.vue
@@ -1,53 +1,54 @@
 <template>
-	<a-modal
-		:visible="visible"
-		@ok="onSubmit"
-		@cancel="closeModal"
-		:maskClosable="false"
-		:keyboard="false"
-		:width="550"
-		:title="title ? '编辑软件模块' : '新增软件模块'"
-	>
-		<a-form :label-col="{ span: 5 }" :wrapper-col="{ span: 14 }" :form="form" v-bind="formItemLayout">
-			<a-form-item label="标题">
-				<a-input
-					placeholder="请输入标题"
-					v-decorator="['title',  {rules:[ { required: true, message: '标题必填!' }]}] "
-				/>
-			</a-form-item>
-			<a-form-item label="副标题">
-				<a-input placeholder="请输入副标题" v-decorator="['subheading']" />
-			</a-form-item>
-			<a-form-item label="内容">
-				<a-textarea placeholder="请输入描述" :rows="4" v-decorator="['content']" />
-			</a-form-item>
-			<a-form-item label="内容标签" extra="标签之间用英文逗号隔开">
-				<a-textarea
-					placeholder="请输入内容标签"
-					:rows="4"
-					v-decorator="['contentDescription',{rules:[{required:true,message:'内容标签必填!'}]}]"
-				/>
-			</a-form-item>
-			<a-form-item label="链接">
-				<a-input placeholder="请输入链接" :rows="4" v-decorator="['link']" />
-			</a-form-item>
-			<a-form-item label="显示顺序">
-				<a-input placeholder="请输入显示顺序" :rows="4" v-decorator="['showIndex']" />
-			</a-form-item>
-			<a-form-item label="附件" extra="此处上传产品板块轮播图">
-				<a-upload
-					v-decorator="['attachment',{valuePropName: 'fileList'}]"
-					action="http://114.67.199.59/cfc/file/upload"
-					list-type="text"
-					:headers="headers"
-				>
-					<a-button type="primary">
-						<a-icon type="upload" />点击上传
-					</a-button>
-				</a-upload>
-			</a-form-item>
-		</a-form>
-	</a-modal>
+  <a-modal
+    :visible='visible'
+    @ok='onSubmit'
+    @cancel='closeModal'
+    :maskClosable='false'
+    :keyboard='false'
+    :width='550'
+    :title="title ? '编辑软件模块' : '新增软件模块'"
+  >
+    <a-form :label-col='{ span: 5 }' :wrapper-col='{ span: 14 }' :form='form' v-bind='formItemLayout'>
+      <a-form-item label='标题'>
+        <a-input
+          placeholder='请输入标题'
+          v-decorator="['title',  {rules:[ { required: true, message: '标题必填!' }]}] "
+        />
+      </a-form-item>
+      <a-form-item label='副标题'>
+        <a-input placeholder='请输入副标题' v-decorator="['subheading']" />
+      </a-form-item>
+      <a-form-item label='内容'>
+        <a-textarea placeholder='请输入描述' :rows='4' v-decorator="['content']" />
+      </a-form-item>
+      <a-form-item label='内容标签' extra='标签之间用英文逗号隔开'>
+        <a-textarea
+          placeholder='请输入内容标签'
+          :rows='4'
+          v-decorator="['contentDescription',{rules:[{required:true,message:'内容标签必填!'}]}]"
+        />
+      </a-form-item>
+      <a-form-item label='链接'>
+        <a-input placeholder='请输入链接' :rows='4' v-decorator="['link']" />
+      </a-form-item>
+      <a-form-item label='显示顺序'>
+        <a-input placeholder='请输入显示顺序' :rows='4' v-decorator="['showIndex']" />
+      </a-form-item>
+      <a-form-item label='附件' extra='此处上传产品板块轮播图'>
+        <a-upload
+          v-decorator="['attachment']"
+          action='http://114.67.199.59/cfc/file/upload'
+          list-type='text'
+          :headers='headers'
+        >
+          <a-button type='primary'>
+            <a-icon type='upload' />
+            点击上传
+          </a-button>
+        </a-upload>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script>
@@ -55,100 +56,100 @@ import ATextarea from 'ant-design-vue/es/input/TextArea'
 import { addSoft, updateSoft } from '@/api/soft.js'
 import Vue from 'vue'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
+import { handleAttachmentId, handleFileList } from '@/utils/constant'
+
 export default {
-	components: { ATextarea },
-	props: {
-		visible: {
-			type: Boolean,
-			required: true
-		},
-		title: {
-			type: Boolean,
-			required: true
-		},
-		data: {
-			type: Object
-		}
-	},
-	data() {
-		return {
-			form: this.$form.createForm(this),
-			formItemLayout: {
-				labelCol: { span: 6 },
-				wrapperCol: { span: 18 }
-			},
-			headers: { authorization: `Bearer  ${Vue.ss.get(ACCESS_TOKEN)}` }
-		}
-	},
-	watch: {
-		visible() {
-			setTimeout(() => {
-				this.getFormData()
-			}, 500)
-		}
-	},
-	methods: {
-		getFormData() {
-			const {
-				title,
-				subheading,
-				content,
-				contentDescription,
-				link,
-				showIndex,
-				attachment
-			} = this.data
-			this.form.setFieldsValue({
-				title,
-				subheading,
-				content,
-				contentDescription,
-				link,
-				showIndex,
-				attachment
-			})
-		},
-		onSubmit() {
-			this.form.validateFields(async (err, values) => {
-				if (err) return
-				if (values.contentDescription == ' ') {
-					return (values.contentDescription = [])
-				} else {
-					values.contentDescription = values.contentDescription
-						.split(',')
-						.filter(item => {
-							if (item != ' ') {
-								return item
-							}
-						})
-				}
-				if (!this.title) {
-					const assParams = {
-						...values,
-						code: 'SOFTWARE_PRODUCT'
-					}
-					const res = await addSoft(assParams)
-					if (res.message == 'success') {
-						this.$message.success('新增成功')
-						this.$emit('ok')
-					}
-				} else {
-					const updParams = {
-						...values,
-						id: this.data.id
-					}
-					const res = await updateSoft(updParams)
-					if (res.message == 'success') {
-						this.$emit('ok')
-						this.$message.success('修改成功')
-					}
-				}
-			})
-		},
-		closeModal() {
-			this.form.resetFields()
-			this.$emit('close')
-		}
-	}
+  components: { ATextarea },
+  props: {
+    visible: {
+      type: Boolean,
+      required: true
+    },
+    title: {
+      type: Boolean,
+      required: true
+    },
+    data: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      form: this.$form.createForm(this),
+      formItemLayout: {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 18 }
+      },
+      headers: { authorization: `Bearer  ${Vue.ss.get(ACCESS_TOKEN)}` }
+    }
+  },
+  watch: {
+    visible () {
+      setTimeout(() => {
+        this.getFormData()
+      }, 500)
+    }
+  },
+  methods: {
+    getFormData () {
+      const {
+        title,
+        subheading,
+        content,
+        contentDescription,
+        link,
+        showIndex,
+        attachment
+      } = this.data
+      const newAttachment = handleFileList(attachment)
+
+      this.form.setFieldsValue({
+        title,
+        subheading,
+        content,
+        contentDescription,
+        link,
+        showIndex,
+        attachment: newAttachment
+      })
+    },
+    onSubmit () {
+      this.form.validateFields(async (err, values) => {
+        if (err) return
+
+        values.attachment = handleAttachmentId(values.attachment.fileList)
+        if (values.contentDescription === ' ') {
+          return (values.contentDescription = [])
+        } else {
+          values.contentDescription = values.contentDescription.split(',').filter(item => Boolean(item))
+        }
+        if (!this.title) {
+          const assParams = {
+            ...values,
+            code: 'SOFTWARE_PRODUCT'
+          }
+          const res = await addSoft(assParams)
+          if (res.message === 'success') {
+            this.$message.success('新增成功')
+            this.$emit('ok')
+          }
+        } else {
+          const updParams = {
+            ...values,
+            id: this.data.id
+          }
+          const res = await updateSoft(updParams)
+          if (res.message === 'success') {
+            this.$emit('ok')
+            this.$message.success('修改成功')
+          }
+        }
+      })
+    },
+    closeModal () {
+      this.form.resetFields()
+      this.$emit('close')
+    }
+  }
 }
 </script>
--- src/views/soft/soft.vue
+++ src/views/soft/soft.vue
@@ -1,21 +1,21 @@
 <template>
-	<div>
-		<a-card title="软件产品" :bordered="false">
-			<a-button @click="handleAdd" type="primary">新增模块</a-button>
-			<a-table style="margin-top: 40px;" :columns="columns" :dataSource="dataSource" rowKey="name"
-				:pagination="false">
-				<span slot="updateTime" slot-scope="updateTime">{{ new Date(updateTime) | formatDate('yyyy-MM-dd') }}</span>
+  <div>
+    <a-card title='软件产品' :bordered='false'>
+      <a-button @click='handleAdd' type='primary'>新增模块</a-button>
+      <a-table style='margin-top: 40px;' :columns='columns' :dataSource='dataSource' rowKey='name'
+               :pagination='false'>
+        <span slot='updateTime' slot-scope='updateTime'>{{ new Date(updateTime) | formatDate('yyyy-MM-dd') }}</span>
 
-				<div slot="actions" slot-scope="record">
-					<a @click="onEditContent(record)">编辑</a>
-					<a-divider type="vertical" />
-					<a @click="onDeleteContent(record.id)">删除</a>
-				</div>
-			</a-table>
-			<soft-modal :visible="visible" :data="formData" :title="title" :module="selectModule" @close="onModalClose"
-				@ok="handleOk"></soft-modal>
-		</a-card>
-	</div>
+        <div slot='actions' slot-scope='record'>
+          <a @click='onEditContent(record)'>编辑</a>
+          <a-divider type='vertical' />
+          <a @click='onDeleteContent(record.id)'>删除</a>
+        </div>
+      </a-table>
+      <soft-modal :visible='visible' :data='formData' :title='title' :module='selectModule' @close='onModalClose'
+                  @ok='handleOk'></soft-modal>
+    </a-card>
+  </div>
 </template>
 
 <script>
@@ -24,130 +24,130 @@ import { tableMixin } from '@/mixins'
 import { getSoftInfoList, deleteSoft } from '@/api/soft.js'
 
 const columns = [
-	{
-		title: '模块名',
-		dataIndex: 'title',
-		align: 'center',
-		width: 100
-	},
-	{
-		title: '显示顺序',
-		dataIndex: 'showIndex',
-		align: 'center',
-		width: 100
-	},
-	{
-		title: '标题',
-		dataIndex: 'title',
-		align: 'center',
-		width: 100
-	},
-	{
-		title: '副标题',
-		dataIndex: 'subheading',
-		align: 'center'
-	},
-	{
-		title: '内容',
-		dataIndex: 'content',
-		align: 'center'
-	},
-	{
-		title: '内容标签',
-		dataIndex: 'contentDescription',
-		align: 'center'
-	},
-	{
-		title: '链接',
-		dataIndex: 'link',
-		align: 'center'
-	},
-	{
-		title: '更新时间',
-		dataIndex: 'createdTime',
-		scopedSlots: { customRender: 'createdTime' },
-		width: 160,
-		align: 'center'
-	},
-	{
-		title: '操作',
-		scopedSlots: { customRender: 'actions' },
-		width: 120,
-		align: 'center',
-		fixed: 'right'
-	}
+  {
+    title: '模块名',
+    dataIndex: 'title',
+    align: 'center',
+    width: 100
+  },
+  {
+    title: '显示顺序',
+    dataIndex: 'showIndex',
+    align: 'center',
+    width: 100
+  },
+  {
+    title: '标题',
+    dataIndex: 'title',
+    align: 'center',
+    width: 100
+  },
+  {
+    title: '副标题',
+    dataIndex: 'subheading',
+    align: 'center'
+  },
+  {
+    title: '内容',
+    dataIndex: 'content',
+    align: 'center'
+  },
+  {
+    title: '内容标签',
+    dataIndex: 'contentDescription',
+    align: 'center'
+  },
+  {
+    title: '链接',
+    dataIndex: 'link',
+    align: 'center'
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'createdTime',
+    scopedSlots: { customRender: 'createdTime' },
+    width: 160,
+    align: 'center'
+  },
+  {
+    title: '操作',
+    scopedSlots: { customRender: 'actions' },
+    width: 120,
+    align: 'center',
+    fixed: 'right'
+  }
 ]
 
 export default {
-	components: { SoftModal },
-	mixins: [tableMixin],
-	data() {
-		return {
-			dataSource: [],
-			formItemLayout: {
-				labelCol: { span: 6 },
-				wrapperCol: { span: 18 }
-			},
-			form: this.$form.createForm(this),
-			visible: false,
-			title: false,
-			formData: {}
-		}
-	},
-	methods: {
-		/* list */
-		async getList() {
-			const code = 'SOFTWARE_PRODUCT'
-			const res = await getSoftInfoList(code)
-			res.map(item => {
-				item.contentDescription = item.contentDescription.join(',')
-				return item
-			})
-			this.dataSource = res
-		},
-		/* 新增Modal */
-		handleAdd() {
-			this.visible = true
-			this.title = false
-		},
-		/* 关闭Modal */
-		onModalClose() {
-			this.title = false
-			this.visible = false
-		},
-		/* 编辑Modal */
-		onEditContent(data) {
-			this.formData = data
-			this.visible = true
-			this.title = true
-		},
-		/* 删除 */
-		async onDeleteContent(id) {
-			const self = this
-			this.$confirm({
-				title: '删除',
-				content: `确定要删除该模块吗？`,
-				okText: '删除',
-				okType: 'danger',
-				async onOk() {
-					await deleteSoft(id)
-					self.$message.success('删除成功')
-					self.getList()
-				},
-				onCancel() {
-					self.$message.warning('取消删除')
-				}
-			})
-		},
-		/* 提交成功 */
-		handleOk() {
-			this.getList()
-			this.onModalClose()
-		}
-	},
-	created() {
-		this.columns = columns
-		this.getList()
-	}
+  components: { SoftModal },
+  mixins: [tableMixin],
+  data () {
+    return {
+      dataSource: [],
+      formItemLayout: {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 18 }
+      },
+      form: this.$form.createForm(this),
+      visible: false,
+      title: false,
+      formData: {}
+    }
+  },
+  methods: {
+    /* list */
+    async getList () {
+      const code = 'SOFTWARE_PRODUCT'
+      const res = await getSoftInfoList(code)
+      res.map(item => {
+        item.contentDescription = item.contentDescription.join(',')
+        return item
+      })
+      this.dataSource = res
+    },
+    /* 新增Modal */
+    handleAdd () {
+      this.visible = true
+      this.title = false
+    },
+    /* 关闭Modal */
+    onModalClose () {
+      this.title = false
+      this.visible = false
+    },
+    /* 编辑Modal */
+    onEditContent (data) {
+      this.formData = data
+      this.visible = true
+      this.title = true
+    },
+    /* 删除 */
+    async onDeleteContent (id) {
+      const self = this
+      this.$confirm({
+        title: '删除',
+        content: `确定要删除该模块吗？`,
+        okText: '删除',
+        okType: 'danger',
+        async onOk () {
+          await deleteSoft(id)
+          self.$message.success('删除成功')
+          self.getList()
+        },
+        onCancel () {
+          self.$message.warning('取消删除')
+        }
+      })
+    },
+    /* 提交成功 */
+    handleOk () {
+      this.getList()
+      this.onModalClose()
+    }
+  },
+  created () {
+    this.columns = columns
+    this.getList()
+  }
 }
 </script>
